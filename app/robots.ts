f7653564a79cfef7f13e905cf049ecf36b1a7134
utkrist Budhathoki <utkristbudhathoki19@gmail.com>
2026-02-11 20:00:36 +0545

    change url

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = "https://utkrist.dev";
+    const baseUrl = "https://utkristbudhathoki.com.np/";
 
     return {
         rules: [
